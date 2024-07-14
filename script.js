9d4104467d406b6b4b43f69e3b6cce4bb45fe1ea
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-07-14 11:36:49 +0530

    Slider Enabled!

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-    },
+const slider = (input) => {
+  const buttons = document.querySelectorAll(".button");
+  buttons.forEach((item) => {
+    item.classList.remove("active_button");
   });
+
+  buttons[input].classList.add("active_button");
+  document.getElementById(input).scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+};
